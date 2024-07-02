f1ce1ae4c168de8aca61745037b6a1adf9a7879e
Chi-chi Wang <chichi@wangchiyi.com>
2024-07-01 20:51:07 -0500

    [app:layouts:Panda] Create layout component `Panda`

---
+++ app/layouts/Panda/__tests__/index.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import useResetScroll from 'app/hooks/useResetScroll';
+
+import Panda from '../index';
+
+function MockedHead() {
+  return (
+    <head>
+      <title>Layout Test</title>
+    </head>
+  );
+}
+
+jest.mock('app/Head', () => MockedHead);
+jest.mock('app/hooks/useResetScroll', () => jest.fn());
+
+function RouterWrappedLayout() {
+  return (
+    <MemoryRouter>
+      <Panda />
+    </MemoryRouter>
+  );
+}
+
+describe('Panda layout', () => {
+  beforeEach(() => {
+    useResetScroll.mockRestore();
+  });
+
+  test('matches no-child snapshot', () => {
+    const tree = renderer.create(<RouterWrappedLayout />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('invokes the useResetScroll hook', () => {
+    renderer.create(<RouterWrappedLayout />);
+
+    expect(useResetScroll).toHaveBeenCalledTimes(1);
+  });
+});
